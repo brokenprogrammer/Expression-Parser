71e994ad3c4e6e03e53ed50373c6ef1491b9799f
Brokenprogrammer <brokenprogrammer@gmail.com>
2016-02-02 18:16:07 +0100

    Added function comments.

--- Expression Parser/Stack.c
+++ Expression Parser/Stack.c
@@ -1,15 +1,47 @@
-//
-//  Stack.c
-//  Expression Parser
-//
-//  Created by Oskar Mendel on 2/2/16.
-//  Copyright © 2016 Oskar Mendel. All rights reserved.
-//
+/**
+ * Expression-Parser.
+ * An Expression parser written in C capable of parsing advanced
+ * math expressions.
+ *
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016 The alg.js Project
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 
 #include "Stack.h"
 #include "stdlib.h"
 
-Stack* Init(int val) {
+/**
+ * initWithData initializes a new Stack by allocating a new pointer with
+ * the ammount of memory a Stack needs. Then after checking if the newly
+ * created stack is NULL or not its values are initialized by using the value
+ * from the parameters and setting the next Stack it points to to NULL.
+ * This initial Stack will serve as the bottom of the stack so it will never
+ * point to another Stack with its next property.
+ *
+ * @param val - Value to place in the Stack structures data property.
+ *
+ * @returns pointer to the just initialized Stack structure(object).
+ */
+Stack* initWithData(int val) {
     Stack *newStack = (Stack *) malloc(sizeof(Stack));
     
     if(newStack == NULL) {
@@ -22,6 +54,17 @@ Stack* Init(int val) {
     return newStack;
 }
 
+/**
+ * push is pushing a new value to the top of the stack. Since the Stack
+ * structure is a LIFO(Last In First Out) the value pushed is stored into a new
+ * Stack structure and the old head Stack (Top) is getting replace with the new
+ * Stack created. That makes it so that the new Stack is the new top pointing 
+ * to the old top.
+ *
+ * @param **head - Pointer to the pointer of head, we use pointer to pointers to
+ * make it easier by letting us change the entire head Stack from this function.
+ * @param val - Value to place in the Stack structures data property.
+ */
 void push(Stack **head, int val) {
     Stack *newStack = (Stack *) malloc(sizeof(Stack));
     
@@ -30,14 +73,25 @@ void push(Stack **head, int val) {
     *head = newStack;
 }
 
+/**
+ * pop is removing the first element of the Stack. Since all Stacks are linked
+ * through the next property pointing to the next Stack in the linked list it
+ * can remove the current top and then make the new top become the next Stack
+ * of the linked list.
+ *
+ * @param **head - Pointer to the pointer of head, we use pointer to pointers to
+ * make it easier by letting us change the entire head Stack from this function.
+ * @param val - Value to place in the Stack structures data property.
+ *
+ * @returns The data value from the removed Stack. Returns -1 if the Stack is
+ * empty.
+ */
 int pop(Stack **head){
-    Stack *newStack = NULL;
-    
-    if (*head == NULL) {
+    if (isEmpty(*head)) {
         return -1;
     }
     
-    newStack = (*head)->next;
+    Stack *newStack = (*head)->next;
     int retval = (*head)->data;
     free(*head);
     *head = newStack;
@@ -45,6 +99,16 @@ int pop(Stack **head){
     return retval;
 }
 
+/**
+ * peek is a function that lets the caller view the value of the element at the
+ * top of the stack without removing it. 
+ *
+ * @param **head - Pointer to the pointer of head, we use pointer to pointers to
+ * make it easier by letting us change the entire head Stack from this function.
+ *
+ * @returns The data value found on the top of the Stack. Returns -1 if the
+ * Stack is empty.
+ */
 int peek(Stack **head) {
     if (!isEmpty(*head)) {
         return (*head)->data;
@@ -52,6 +116,13 @@ int peek(Stack **head) {
     return -1;
 }
 
+/**
+ * isEmpty checks if the Stack passed into the parameters is empty.
+ *
+ * @param *head - The Stack to check if empty or not.
+ *
+ * @returns 1 if the Stack is empty. Returns 0 if the Stack is not empty.
+ */
 int isEmpty(Stack *head) {
     
     if (head == NULL) {
@@ -61,7 +132,13 @@ int isEmpty(Stack *head) {
     return 0;
 }
 
-void Display(Stack *head) {
+/**
+ * display loops through the entire Stacks linked list priting out every
+ * data property found while looping through the entire list of linked Stacks.
+ *
+ * @param *head - The target Stack to loop through its linked elements.
+ */
+void display(Stack *head) {
     Stack *current = head;
     
     while (current != NULL) {
--- Expression Parser/main.c
+++ Expression Parser/main.c
@@ -1,10 +1,30 @@
-//
-//  main.c
-//  Expression Parser
-//
-//  Created by Oskar Mendel on 2/2/16.
-//  Copyright © 2016 Oskar Mendel. All rights reserved.
-//
+/**
+ * Expression-Parser.
+ * An Expression parser written in C capable of parsing advanced
+ * math expressions.
+ *
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016 The alg.js Project
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 
 #include <stdio.h>
 #include "stdlib.h"
@@ -14,13 +34,13 @@ int main(int argc, const char * argv[]) {
     // insert code here....
     printf("Hello, World!\n");
     
-    Stack *root = Init(5);
+    Stack *root = initWithData(5);
     
     push(&root, 10);
     push(&root, 15);
     push(&root, 20);
     
-    Display(root);
+    display(root);
     
     printf("Popping top of the stack: %i\n", pop(&root));
     printf("Popping top of the stack: %i\n", pop(&root));
@@ -29,7 +49,7 @@ int main(int argc, const char * argv[]) {
     printf("Popping top of the stack: %i\n", pop(&root));
     printf("Popping top of the stack: %i\n", pop(&root));
     
-    Display(root);
+    display(root);
     if(isEmpty(root)) {
         printf("The Stack is empty\n");
     }
